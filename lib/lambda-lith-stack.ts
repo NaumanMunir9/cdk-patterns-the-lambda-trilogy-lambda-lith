e95a521e6656f715bfb6baa771b58b206adc3287
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-01 20:25:04 +0500

    Lambda Function

--- lib/lambda-lith-stack.ts
+++ lib/lambda-lith-stack.ts
@@ -1,16 +1,19 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class LambdaLithStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaLithQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // =====================================================================
+    // Lambda Function
+    // =====================================================================
+    const lambdaFunction = new lambda.Function(this, "LambdaLith", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "main.handler",
+    });
   }
 }
