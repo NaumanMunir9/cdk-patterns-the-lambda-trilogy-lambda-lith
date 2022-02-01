44926700337640bd0b9f387e8bcace92f13c9394
NaumanMunir9 <naumanmunir9@gmail.com>
2022-02-01 18:10:18 +0500

    Initial commit

---
+++ test/lambda-lith.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaLith from '../lib/lambda-lith-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-lith-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaLith.LambdaLithStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
